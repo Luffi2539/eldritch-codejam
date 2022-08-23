000792770919dac649efb4813d8f4ae76e3597f6
Maestro <akievaleksndr@mail.ru>
2022-08-23 17:27:31 +0500

    feat: change stages and difficulty

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// ---------------------------------------
+
 const rain = document.querySelector('.rain');
 
 const knife = new Audio('./assets/Audio/6802dff441a6d4e (mp3cut.net).mp3');
@@ -47,7 +49,108 @@ monster4.forEach(card => {
 
 rain.volume = 0;
 knife.volume = .1;
-scream1.volume = .5;
-scream2.volume = .5;
-scream3.volume = .5;
-scream4.volume = .5;
+scream1.volume = .0;
+scream2.volume = .0;
+scream3.volume = .0;
+scream4.volume = .0;
+
+// ---------------------------------------
+
+// ---------------------------------------
+
+const card1 = document.querySelector('.card1');
+const card2 = document.querySelector('.card2');
+const card3 = document.querySelector('.card3');
+const card4 = document.querySelector('.card4');
+
+const cards = document.querySelectorAll('.ancient-cards img')
+
+const deck = document.querySelector('.deck-of-cards');
+
+cards.forEach(card => {
+    card.addEventListener('click', () => {
+        cards.forEach(card => card.classList.remove('fire-border'));
+        card.classList.add('fire-border');
+    })
+});
+
+const difficulty = document.querySelectorAll('.difficulty')
+
+difficulty.forEach(item => {
+    item.addEventListener('click', () => {
+        difficulty.forEach(item => item.classList.remove('fire-border'));
+        item.classList.add('fire-border');
+        deck.classList.add('show');
+    })
+});
+
+// ---------------------------------------
+
+// ---------------------------------------
+
+const firstGreen = document.querySelector('.first-green');
+const firstBrown = document.querySelector('.first-brown');
+const firstBlue = document.querySelector('.first-blue');
+const secondGreen = document.querySelector('.second-green');
+const secondBrown = document.querySelector('.second-brown');
+const secondBlue = document.querySelector('.second-blue');
+const thirdGreen = document.querySelector('.third-green');
+const thirdBrown = document.querySelector('.third-brown');
+const thirdBlue = document.querySelector('.third-blue');
+
+const difficultyBlock = document.querySelector('.difficulty-level');
+
+card1.addEventListener('click', () => {
+    firstGreen.value = 0;
+    firstBrown.value = 2;
+    firstBlue.value = 2;
+    secondGreen.value = 1;
+    secondBrown.value = 3;
+    thirdGreen.value = 3;
+    thirdBrown.value = 4;
+    difficultyBlock.classList.add('show');
+});
+
+card2.addEventListener('click', () => {
+    firstGreen.value = 1;
+    firstBrown.value = 2;
+    firstBlue.value = 1;
+    secondGreen.value = 3;
+    secondBrown.value = 2;
+    secondBlue.value = 1;
+    thirdGreen.value = 2;
+    thirdBrown.value = 4;
+    thirdBlue.value = 0;
+    difficultyBlock.classList.add('show');
+});
+
+card3.addEventListener('click', () => {
+    firstGreen.value = 0;
+    firstBrown.value = 2;
+    firstBlue.value = 1;
+    secondGreen.value = 2;
+    secondBrown.value = 3;
+    secondBlue.value = 1;
+    thirdGreen.value = 3;
+    thirdBrown.value = 4;
+    thirdBlue.value = 0;
+    difficultyBlock.classList.add('show');
+});
+
+card4.addEventListener('click', () => {
+    firstGreen.value = 1;
+    firstBrown.value = 2;
+    firstBlue.value = 1;
+    secondGreen.value = 2;
+    secondBrown.value = 3;
+    secondBlue.value = 1;
+    thirdGreen.value = 2;
+    thirdBrown.value = 4;
+    thirdBlue.value = 0;
+    difficultyBlock.classList.add('show');
+});
+
+// ---------------------------------------
+
+// ---------------------------------------
+
